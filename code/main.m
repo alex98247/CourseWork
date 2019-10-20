cdf6fd203146c252f1ed82a1fcce544cf02b5638
Alex <alexmir2015@yandex.ru>
2019-10-20 12:43:00 +0300

    use transient function

--- code/main.m
+++ code/main.m
@@ -1,16 +1,25 @@
-ddeoptions=ddeset('RelTol', 1e-8, 'AbsTol', 1e-8);
-sol = ode23(@ddun, [0, 0.001], [1, 0, 0], ddeoptions);
+global A b c h Kvco omegaEFree
+
+taoP1=10^(-3);
+taoZ1=10^(-4);
+Kvco=250;
+omegaEFree = 0;
+numerator = [taoZ1^2, 2*taoZ1, 1];
+denominator = [taoP1^2, 2*taoP1, 1];
+G = tf(numerator,denominator);
+[A,b,c,h] = ssdata(G);
+disp(A);
+disp(b);
+disp(c);
+disp(h);
+ddeoptions=ddeset('RelTol', 1e-8, 'AbsTol', 1e-15);
+sol = ode23(@ddun, [0, 2*pi], [0, 0, 0.01], ddeoptions);
 plot3(sol.y(1, :),sol.y(2, :),sol.y(3, :));
 %------------------------------------------------------------------
 function dydt = ddun(t,y)
-Kvco=250;
-taop1=10^(-3);
-taoz1=10^(-4);
-b=[0,1/taop1^2];
-c=[(1/taop1^2 - taoz1^2/taop1^4),2*(taoz1/taop1^2 - taoz1^2/taop1^3)];
-A=[0,1; 
-   1/taop1^2, 2/taop1];
+global A b c h Kvco omegaEFree
+
 dydt=zeros(3,1);
-dydt(1:2)=A*y(1:2) + (b*y(3))';
-dydt(3)=0-Kvco*(c*y(1:2)+(taoz1^2/taop1^2)*sin(y(3)));
+dydt(1:2)=A*y(1:2) + b*sin(y(3));
+dydt(3)=omegaEFree-Kvco*(c*y(1:2)+h*sin(y(3)));
 end
