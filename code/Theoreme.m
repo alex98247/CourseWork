66f1b2c85ea645b75484b497486d3daec162aa29
alex <alexmir2015@yandex.ru>
2019-11-09 13:18:01 +0300

    add simple filter

--- code/Theoreme.m
+++ code/Theoreme.m
@@ -1,10 +1,16 @@
 taoP1=10^(-3);
 taoZ1=10^(-4);
 
-numerator = [1, -2*taoZ1, taoZ1^2];
-denominator = [1, -2*taoP1, taoP1^2];
+hi=1;
+eps=0.002;
+tau=5;
+delta=1;
+
+numerator = [1, 2*taoZ1, taoZ1^2];
+denominator = [1, 2*taoP1, taoP1^2];
 K = poly(numerator)/poly(denominator);
 
 X = -0.25:10^(-5):0.25;
 K = @(x) polyval(numerator, x)./polyval(denominator, x);
-plot(real(K(X*i)));
+plot(real(hi*K(X*i)-eps*conj(K(X*i)).*K(X*i)-tau*(conj(K(X*i))+X*i).*(K(X*i) + X*i)));
+disp(4*eps*delta/hi^2);
