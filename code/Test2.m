2015ee78074659290991862f8563036a218b3ebb
alex <alexmir2015@yandex.ru>
2019-12-09 01:09:24 +0300

    fix

--- code/Test2.m
+++ code/Test2.m
@@ -9,7 +9,7 @@ Ds = subs(D, p, 1);
 Ds1 = subs(Ds, s, (1-r)/2);
 disp(simplify(Ds1));
 
-K = (1+2*a*x+a ^2*x^2)/(1+2*b*x+b^2*x^2);
+K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
 f1 = conj(Ki-1i*x)*(Ki+1i*x);
@@ -17,6 +17,14 @@ f2 = p*Ki - q*f - r*f1 - s;
 f3 = subs(f2, r, 0);
 f3 = subs(f3, a, ai);
 f3 = subs(f3, b, bi);
+disp(simplify(real(f3)));
+
+K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
+Ki = subs(K, x, 1i*x);
+f = Ki*conj(Ki);
+f1 = conj(Ki-1i*x)*(Ki+1i*x);
+f2 = p*Ki - q*f - r*f1 - s;
+f3 = subs(f2, r, 0);
 disp(simplify(real(f3)));
 
 end
--- code/filter2/filter2_1.m
+++ code/filter2/filter2_1.m
@@ -1,7 +1,7 @@
-function filter2()
+function filter2_1()
 x = sym('x');
 
-%For tau = 0
+%For tau > 0
 [taoP1,taoP2] = meshgrid(0:0.01:1,0:0.01:1);
 grid = [taoP1(:) taoP2(:)];
 Kvco = 1000;
@@ -13,7 +13,7 @@ M = [];
 A = [];
 B = [];
 fun = @(x)-(4*x(1)*x(2)/(x(3))^2);
-x0 = [0.5, 0.05, 1];
+x0 = [0.5, 0.05, 2];
 lb = [0.00000001,0.0001, 000001];
 ub = [10000,10000,10000];
 AA = [];
@@ -28,21 +28,20 @@ for i=1:length(a)
 
     if(ai ~= bi)
         M1 = fmincon(fun,x0,AA,b1,Aeq,beq,lb,ub,@con,options);
-        if((-ai^4*M1(1)- bi^4*M1(2)+ ai^2*bi^2*M1(3)) > 0)
-            M(i) = -fun(M1);
-            A(i) = ai;
-            B(i) = bi;
-            M(i) = Kvco*vpasolve(0.5*pi*x/(x*asin(x)+sqrt(1-x^2)) - M(i));
-            disp(i/length(a));
-        end
+        M(i) = -fun(M1);
+        A(i) = ai;
+        B(i) = bi;
+        M(i) = Kvco*vpasolve(0.5*pi*x/(x*asin(x)+sqrt(1-x^2)) - M(i));
+        disp(i/length(a));
     end
 end
 
 plot3(A, B, M);
 
 function [c,ceq] = con(x)
-c(1) = (ai^2*x(3)+ 2*ai^2*x(1)+ bi^2*x(3)+ 2*bi^2*x(2)- 4*ai*bi*x(3))^2-4*(ai^4*x(1)+ bi^4*x(2)- ai^2*bi^2*x(3))*(x(1) - x(3) + x(2));
-c(2) = -(-ai^4*x(1)- bi^4*x(2)+ ai^2*bi^2*x(3));
+c(1) = -(-ai^4*x(2) - bi^4 + 2*ai^2- ai^4*x(1) + bi^2*ai^2*x(3));
+c(2) = -(1-bi^2*x(3) - 2*bi^2*x(2) - ai^2*x(3)- 2*bi^2+ 4*bi*ai*x(3)- 2*ai^2*x(1));
+c(3) = -(x(3)-x(2)-1-x(1));
 ceq = [];
 end
 end
