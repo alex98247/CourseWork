1a6de9ee186293777fcd3e7ca03ce40081fa52eb
alex <alexmir2015@yandex.ru>
2019-12-26 11:09:12 +0300

    filter 2

--- code/Test2.m
+++ code/Test2.m
@@ -1,5 +1,5 @@
 function Test2()
-syms x a b c d p q r s ai bi real
+syms x a b c d p q r t s ai bi real
 %p = kappa
 %q = epsolon
 %r = tau
@@ -9,15 +9,12 @@ Ds = subs(D, p, 1);
 Ds1 = subs(Ds, s, (1-r)/2);
 disp(simplify(Ds1));
 
-K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
+K = 1/((1+a*x)*(1+b*x)*(1+c*x));
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
 f1 = conj(Ki-1i*x)*(Ki+1i*x);
 f2 = p*Ki - q*f - r*f1 - s;
-f3 = subs(f2, r, 0);
-f3 = subs(f3, a, ai);
-f3 = subs(f3, b, bi);
-disp(simplify(real(f3)));
+disp(simplify(real(f2)));
 
 K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
 Ki = subs(K, x, 1i*x);
