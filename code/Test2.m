2f04735db9cea66eb6daec23ebcb42eacb48bd07
Alex Mironov <bermar2016@yandex.ru>
2020-01-25 00:43:18 +0300

    add filter 3 text

--- code/Test2.m
+++ code/Test2.m
@@ -1,5 +1,5 @@
 function Test2()
-syms x a b c d p q r t s ai bi real
+syms x a b c d p q r t s ai bi a1 a2 b1 b2 real
 %p = kappa
 %q = epsolon
 %r = tau
@@ -9,23 +9,25 @@ Ds = subs(D, p, 1);
 Ds1 = subs(Ds, s, (1-r)/2);
 %disp(simplify(Ds1));
 
-K = ((1+a*b*x)*(1+c*d*x))/((1+a*x)*(1+d*x));
+%filter 3, tau = 0
+K = (1+a1*b1*x+a2*b2*x^2)/(1+a1*x+a2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
 f1 = conj(Ki-1i*x)*(Ki+1i*x);
 f2 = p*Ki - q*f - r*f1 - s;
 f3 = subs(f2, r, 0);
-disp(simplify(real(f3)));
+disp(collect(simplify(real(f3))));
 
-K = ((1+a*b*x+c*d*x^2))/((1+a*x+d*x^2));
+%filter 3, tau = 0, kappa = 1, epsilon = 1 - delta
+K = (1+a1*b1*x+a2*b2*x^2)/(1+a1*x+a2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
 f1 = conj(Ki-1i*x)*(Ki+1i*x);
 f2 = p*Ki - q*f - r*f1 - s;
 f3 = subs(f2, r, 0);
-f3 = subs(f3, s, 1-s);
-f3 = subs(f3, p, 1);
-disp(simplify(real(f3)));
+f4 = subs(f3, p, 1);
+f5 = subs(f4, q, 1-s);
+disp(collect(simplify(real(f5))));
 
 K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
 Ki = subs(K, x, 1i*x);
