61dd1ea79f43f559eaabcea564c182e327f3e008
Alex Mironov <bermar2016@yandex.ru>
2020-02-07 22:35:36 +0300

    fix filter2

--- code/Test2.m
+++ code/Test2.m
@@ -9,7 +9,17 @@ Ds = subs(D, p, 1);
 Ds1 = subs(Ds, s, (1-r)/2);
 %disp(simplify(Ds1));
 
+%filter 3, tau not null
+disp('filter 3, tau not null');
+K = (1+a1*b1*x+a2*b2*x^2)/(1+a1*x+a2*x^2);
+Ki = subs(K, x, 1i*x);
+f = Ki*conj(Ki);
+f1 = conj(Ki-1i*x)*(Ki+1i*x);
+f2 = p*Ki - q*f - r*f1 - s;
+disp(collect(simplify(real(f2))));
+
 %filter 3, tau = 0
+disp('filter 3, tau = 0');
 K = (1+a1*b1*x+a2*b2*x^2)/(1+a1*x+a2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
@@ -19,6 +29,7 @@ f3 = subs(f2, r, 0);
 disp(collect(simplify(real(f3))));
 
 %filter 3, tau = 0, kappa = 1, epsilon = 1 - delta
+disp('filter 3, tau = 0, kappa = 1, epsilon = 1 - delta');
 K = (1+a1*b1*x+a2*b2*x^2)/(1+a1*x+a2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
@@ -29,13 +40,17 @@ f4 = subs(f3, p, 1);
 f5 = subs(f4, q, 1-s);
 disp(collect(simplify(real(f5))));
 
+%filter2, tau = 0, kappa = 1, epsilon = 1 - delta
+disp('filter2, tau = 0, kappa = 1, epsilon = 1 - delta');
 K = (1+2*a*x+a^2*x^2)/(1+2*b*x+b^2*x^2);
 Ki = subs(K, x, 1i*x);
 f = Ki*conj(Ki);
 f1 = conj(Ki-1i*x)*(Ki+1i*x);
 f2 = p*Ki - q*f - r*f1 - s;
 f3 = subs(f2, r, 0);
-%disp(simplify(real(f3)));
+f4 = subs(f3, p, 1);
+f5 = subs(f4, q, 1-s);
+disp(collect(simplify(real(f5))));
 
 
 f = 4*s*(1 - b) - 4*s^2*(a^2 -2*b +1);
@@ -43,6 +58,7 @@ f = subs(f, s, (1-b)/(2*(a^2 - 2*b +1)));
 %disp(simplify(real(f)));
 
 %Discriminant for filter 2
+disp('Discriminant for filter 2');
 f = (4*a*b-a^2-2*a^2*q-b^2-2*b^2*s)^2-4*(a^2*b^2-a^4*q-b^4*s)*(1-q-s);
-disp(simplify(f));
+disp(expand(simplify(f)));
 end
