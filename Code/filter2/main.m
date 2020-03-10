d088ebd8ff41da93addcd1412b2c7d18e8a83e86
alex <alexmir2015@yandex.ru>
2020-03-10 23:42:20 +0300

    article

--- Code/filter2/main.m
+++ Code/filter2/main.m
@@ -1,8 +1,16 @@
 [M1, A1, B1] = filter2_tau0_2e();
 [M2, A2, B2] = filter2_tau0_1();
 
+for i=1:length(A1)
+    A11(i) = (A1(i)-B1(i))/2;
+    B11(i) = -A11(i);
+    
+    A21(i) = (A2(i)-B2(i))/2;
+    B21(i) = -A21(i);
+end
+
 %Show results of exact and calculated values nu^2 
-plot3(A1, B1, M1, '.', A2, B2, M2, '.');
+plot(A21, M2, '.');
 axis equal
 xlabel('tauP1')
 ylabel('tauZ1')
